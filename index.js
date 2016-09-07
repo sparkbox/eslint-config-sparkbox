c7f886621b2c35b32acfb3bf00577bab856099fe
Adam Simpson <adam@heysparkbox.com>
2016-09-07 14:35:26 -0400

    feat: initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+module.exports = {
+    "extends": "airbnb",
+    "installedESLint": true,
+    "parser": "babel-eslint",
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "import",
+        "flowtype"
+    ],
+    "env": {
+      "node": true,
+      "browser": true
+    },
+    "rules": {
+      "react/jsx-filename-extension": [0, { "extensions": [".js", ".jsx"] }],
+      "react/require-extension": 0,
+      "flowtype/define-flow-type": 1,
+      "flowtype/use-flow-type": 1,
+      "arrow-body-style": [2, "always"],
+      "react/no-did-mount-set-state": 0,
+      "import/no-mutable-exports": 0,
+      "import/no-extraneous-dependencies": 0,
+      "no-console": 0,
+      "strict": 0,
+      "new-cap": [2, {"capIsNewExceptions": ["Router"]}]
+    }
+}
