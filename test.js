e9ffdf3cdd2eb432fbd70595e01d2e3c9bc73371
Adam Simpson <adam@heysparkbox.com>
2019-02-15 13:41:46 -0500

    test: :white_check_mark: add basic tests for react and non-react js

---
+++ test.js
@@ -0,0 +1,43 @@
+const assert = require('assert');
+const Engine = require('eslint').CLIEngine;
+
+const cli = new Engine({
+  configFile: 'index.js',
+});
+
+const lint = text => cli.executeOnText(text);
+
+describe('Non-react JS', function() {
+  it('Will pass', function() {
+    const test = lint(`
+const foo = 'foo';
+window.location(foo);\n`);
+
+    const message = test.errorCount > 0 && test.results[0].messages[0].message;
+
+    assert.equal(test.errorCount, 0, message);
+  });
+});
+
+describe('React JS', function() {
+  it('Will pass', function() {
+    const test = cli.executeOnText(`
+import React, { Component } from 'react';
+
+export class Foo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <p>Foo</p>;
+  }
+}
+`);
+
+    const message = test.errorCount > 0 && test.results[0].messages[0].message;
+
+    assert.equal(test.errorCount, 0, message);
+  });
+});
