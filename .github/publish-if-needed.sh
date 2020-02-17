534400d0798626314d0d715fe81c9247c3242ccf
Jordan Leven <jordan@heysparkbox.com>
2020-02-17 16:16:24 -0500

    :bug: fix: Update publish action
     - GitHub actions were running into errors with line 9 of this file. Since we're running the comparison inside of the test `[` command, it should use a single `=`.

--- .github/publish-if-needed.sh
+++ .github/publish-if-needed.sh
@@ -4,9 +4,9 @@
 
 PACKAGE_NAME=$(node -e "process.stdout.write(require('./package.json').name)")
 PACKAGE_VERSION=$(node -e "process.stdout.write(require('./package.json').version)")
-EXISTING_PACKAGE=$(npm info $PACKAGE_NAME versions --json | jq --arg PACKAGE_VERSION $PACKAGE_VERSION 'index($PACKAGE_VERSION)')
+EXISTING_PACKAGE=$(npm info "$PACKAGE_NAME" versions --json | jq --arg PACKAGE_VERSION "$PACKAGE_VERSION" 'index($PACKAGE_VERSION)')
 
-if [ "$EXISTING_PACKAGE" == "null" ];
+if [ "$EXISTING_PACKAGE" = "null" ];
 then
   npm publish
 else
